da978d112490da16ea3d6c37e4b9b330d950973c
fifth-island <113310477+fifth-island@users.noreply.github.com>
2022-10-20 10:24:54 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// var bgWidth = 350; //Max-width of <li> you would like to set.
+//     var scrollPos = Math.ceil($('body').width() / 20);
+//     $(document).ready(function() {
+//       $('body').width(bgWidth + $(window).width());
+//       $(window).scroll(function() {
+//         if ($(window).scrollLeft() >= ($('body').width() - $(window).width())) {
+//           $(window).scrollLeft(1 + scrollPos / 4);
+//         } else if ($(window).scrollLeft() == 0) {
+//           $(window).scrollLeft($('body').width() - $(window).width() - scrollPos / 4);
+//         }
+//       });
+//     });
+//     $(window).resize(function() {
+//       $('body').width(bgWidth + $(window).width());
+//     });
