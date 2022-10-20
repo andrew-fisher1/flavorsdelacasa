aa70e58ebe3eef38e3126e4792bf0a2d69bf0bba
fifth-island <113310477+fifth-island@users.noreply.github.com>
2022-10-20 10:54:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,3 +13,12 @@
 //     $(window).resize(function() {
 //       $('body').width(bgWidth + $(window).width());
 //     });
+
+// fades in main elements of the page
+ $(document).ready(function() {
+  // $("main-logo, main-logo").pagefade(1000, 1000);
+  $('#main-logo').hide().fadeIn(3000);
+  $('#main-text').hide().fadeIn(3000);
+
+
+  });
