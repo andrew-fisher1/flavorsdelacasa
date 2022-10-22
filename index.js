dd22554e5fcb40aca922f0364c73ad00b2a83eb0
fifth-island <113310477+fifth-island@users.noreply.github.com>
2022-10-22 19:29:04 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-// fades in main elements of the page
- $(document).ready(function() {
-  // $("main-logo, main-logo").pagefade(1000, 1000);
-  $('#main-logo').hide().fadeIn(3000);
-  $('#main-text').hide().fadeIn(3000);
-
-
-  });
-  
